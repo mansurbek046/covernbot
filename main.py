a6be72dd115932487c6c252befc0cd1f4c5ec82e
mansurbek046 <mansurbek046@gmail.com>
2024-04-16 06:51:22 +0500

    .

--- main.py
+++ main.py
@@ -37,16 +37,16 @@ async def incoming(client, message):
             elif len(url.split('.'))>=2:
                 url=f'https://{url}'
             else:
+                url=f"https://duckduckgo.com?q={message.text.lower().replace(' ','%20')}"
+                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Results', url=WebAppInfo(url=url))]])
+                await client.send_message(chat_id=chat_id, text='DuckDuckGo search!', reply_markup=reply_markup)
                 break
             print(url)
             reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Click me!', url=WebAppInfo(url=url))]])
-            await client.send_message(chat_id=chat_id, text='Click button:')
+            await client.send_message(chat_id=chat_id, text='Click button:', reply_markup=reply_markup)
 
         else:
-            url=f"https://duckduckgo.com?q={message.text.lower().replace(' ','%20')}"
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('Results', url=WebAppInfo(url=url))]])
-            await client.send_message(chat_id=chat_id, text='DuckDuckGo search!')
-
+            
 if __name__=='__main__':
     app.run()
 
